cd59bc0fd3a71987616140a06867daa1c21558e7
pkd2512 <pkd.dapacific@gmail.com>
2022-09-15 22:20:22 +0530

    simple scraper setup for json and csv files

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
-const run = () => {
-  console.log("main code here");
+import { fetchCSV, fetchJSON } from "./lib/fetchData.js";
+import { writeCSV, writeJSON } from "./lib/writeData.js";
+
+import formatData from "./lib/formatData.js";
+
+/**
+ * This is the main function where the scraper begins
+ * The scraper fetches data, formats it in some way and saves it locally in GitHub
+ * The helper methods are in lib directory
+ */
+const run = async() => {
+
+  /* ----------------------------------- */
+  /* STEP 1: Fetch the data from the url */
+  /* ----------------------------------- */
+
+  const DATA = await fetchJSON("https://raw.githubusercontent.com/stefangabos/world_countries/master/data/countries/_combined/countries.json");
+  const DATA_CSV = await fetchCSV("https://raw.githubusercontent.com/owid/owid-datasets/master/datasets/Air%20Pollutant%20Emissions%20-%20OECD/Air%20Pollutant%20Emissions%20-%20OECD.csv");
+
+  /* ----------------------------------------------------------------- */
+  /* STEP 2: Format the data. There can be as many methods as required */
+  /* ----------------------------------------------------------------- */
+
+  const FORMATTED_DATA = await formatData(DATA);
+
+  /* ---------------------------------------------- */
+  /* STEP 3: Save your data locally or on the cloud */
+  /* ---------------------------------------------- */
+
+  await writeJSON(FORMATTED_DATA, 'database/data.json');
+  await writeCSV(DATA_CSV, 'database/data.csv');
 }
 
 run();
